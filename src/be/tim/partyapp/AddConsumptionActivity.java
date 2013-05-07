cfc935571d1b69664fc1131acb2f635537601283
tim <tim@tim.be>
2013-05-07 20:17:29 +0200

    extra beverages added, seperate screen to add & overview

---
+++ src/be/tim/partyapp/AddConsumptionActivity.java
@@ -0,0 +1,11 @@
+package be.tim.partyapp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tim
+ * Date: 07/05/13
+ * Time: 19:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class AddConsumptionActivity {
+}
---
+++ src/be/tim/partyapp/model/ConsumptionType.java
@@ -0,0 +1,11 @@
+package be.tim.partyapp.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tim
+ * Date: 07/05/13
+ * Time: 19:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConsumptionType {
+}
